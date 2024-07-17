aebe4eb441660f34077fbecd946dc23a8066bfe3
Adam Hebert <adam.hebert656@gmail.com>
2024-07-17 11:19:20 -0500

    Merge changes

--- lua/options.lua
+++ lua/options.lua
@@ -78,3 +78,19 @@ vim.g.root_spec = { ".git" }
 
 vim.opt.confirm = true
 vim.opt.smoothscroll = true
+
+-- LSP Options
+local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+  group = vim.api.nvim_create_augroup("float_diagnostic_cursor", { clear = true }),
+  callback = function ()
+    vim.diagnostic.open_float(nil, {focus=false, scope="cursor"})
+  end
+})
+
+
