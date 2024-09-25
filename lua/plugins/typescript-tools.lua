1f0f04baf382074ae1be2142006ed1f2c62fa10a
Adam Hebert <adam.hebert656@gmail.com>
2024-09-25 13:29:25 -0500

    Small changes

--- lua/plugins/typescript-tools.lua
+++ lua/plugins/typescript-tools.lua
@@ -9,6 +9,12 @@ return {
       separate_diagnostic_server = true,
       -- "change"|"insert_leave" determine when the client asks the server about diagnostic
       publish_diagnostic_on = "insert_leave",
+      tsserver_plugins = {
+        -- for TypeScript v4.9+
+        "@styled/typescript-styled-plugin",
+        -- or for older TypeScript versions
+        -- "typescript-styled-plugin",
+      },
     },
     root_dir = util.root_pattern('.git', 'tsconfig.json', 'jsconfig.json', 'package.json'),
   },
