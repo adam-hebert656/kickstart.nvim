3f21d02efdfe9b4167a9c486a4b57c8fc946788e
Adam Hebert <adam.hebert656@gmail.com>
2025-02-20 17:40:44 -0600

    Remove unneeded config for noice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -2,14 +2,6 @@ return {
   "folke/noice.nvim",
   event = "VeryLazy",
   opts = {
-    messages = {
-      enabled = true, 
-      view = "notify", -- default view for messages
-      view_error = "notify", -- view for errors
-      view_warn = "notify", -- view for warnings
-      view_history = "messages", -- view for :messages
-      view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
-    },
     lsp = {
       override = {
         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
